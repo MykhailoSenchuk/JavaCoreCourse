e6f5fb5c28ea5ec1d29ab70f25376f2949c608d0
Mykhailo <mhs.senchuk@gmail.com>
2016-08-15 20:02:26 +0300

    Homework commit

---
+++ src/Module1/FifthClass.java
@@ -0,0 +1,10 @@
+package Module1;
+
+/**
+ * Created by Mykhailo on 8/15/2016.
+ */
+public class FifthClass {
+    public static void main(String[] args) {
+        System.out.println("Fifth class in Java");
+    }
+}
---
+++ src/Module1/FirstClass.java
@@ -0,0 +1,10 @@
+package Module1;
+
+/**
+ * Created by Mykhailo on 8/15/2016.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("First class in Java");
+    }
+}
