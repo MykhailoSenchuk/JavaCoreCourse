1ab6e0562db93e0aaf9e12aa907c0cc30082176b
Mykhailo <mhs.senchuk@gmail.com>
2016-09-01 19:22:43 +0300

    Homework 2 added check for division by zero and comments

--- src/module2/hometask/Task1.java
+++ src/module2/hometask/Task1.java
@@ -5,15 +5,19 @@ package module2.hometask;
  */
 public class Task1 {
     public static void main(String[] args) {
-        int[] values = {-20, -20, -5, -4, -5, -4, -7, -10, 10, 10};
+        int[] values = {0, -20, -5, -4, -5, -4, 0,0,0,0};
         System.out.println("Sum is " + sum(values));
         System.out.println("Min is " + min(values));
         System.out.println("Max is " + max(values));
         System.out.println( ( maxPositive(values) != 0 ) ? ( "Max positive is " + maxPositive(values) ) : "There are no positive numbers");
         System.out.println("Multiplication is " + multiplication(values) );
+        if(values[values.length - 1] != 0)
+            System.out.println("modulus of first and last element is " + modulus(values) );
+        else
+            System.out.println("modulus:Division by zero");
         System.out.println("Second largest is " + secondLargest(values) );
-
     }
+
     public static int sum(int array[]) {
         int sum = 0;
         for(int item : array) {
@@ -106,6 +110,7 @@ public class Task1 {
     public static double modulus(double array[]){
         return array[0] % array[array.length - 1];
     }
+    //descending sort
     public static void sort( int array[] ) {
         boolean swap = true;
         int buffer = 0;
@@ -121,24 +126,27 @@ public class Task1 {
                 }
             }
         }
+    //descending sort
     public static void sort( double array[] ) {
-        boolean swap = true;
-        double buffer = 0;
-        while (swap) {
-            swap = false;
-            for(int i = 0;  i < array.length - 1;  i++) {
-                if ( array[i] < array[i+1] ) {
-                    buffer = array[i];
-                    array[i] = array[i+1];
-                    array[i+1] = buffer;
-                    swap = true;
-                }
+    boolean swap = true;
+    double buffer = 0;
+    while (swap) {
+        swap = false;
+        for(int i = 0;  i < array.length - 1;  i++) {
+            if ( array[i] < array[i+1] ) {
+                buffer = array[i];
+                array[i] = array[i+1];
+                array[i+1] = buffer;
+                swap = true;
             }
         }
     }
+    }
     public static int secondLargest(int array[]){
-        sort(array);
+        sort(array);//receive descending array
         int i;
+        //go through descending array starting from second element(skipping max element)
+        // to find second largest number
         for(i = 1;  i < array.length;  i++) {
             if(array[i] != array [i-1]){
                 break;
@@ -147,8 +155,10 @@ public class Task1 {
         return array[i];
     }
     public static double secondLargest(double array[]){
-        sort(array);
+        sort(array);//receive descending array
         int i;
+        //go through descending array starting from second element(skipping max element)
+        //to find second largest element
         for(i = 1;  i < array.length;  i++) {
             if(array[i] != array [i-1]){
                 break;
