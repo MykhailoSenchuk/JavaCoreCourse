e52405f62db1c914eacff5ad948897aa3ccd0fe0
Mykhailo <mhs.senchuk@gmail.com>
2016-09-16 12:58:38 +0300

    Homework 3 task 2 change

---
+++ src/module3/hometask/task3_2/Solution.java
@@ -0,0 +1,15 @@
+package module3.hometask.task3_2;
+
+/**
+ * Created by Mykhailo on 9/16/2016.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 2;
+
+        Adder test = new Adder();
+        System.out.println(test.add(a,b));
+        System.out.println(test.check(a,b));
+    }
+}
